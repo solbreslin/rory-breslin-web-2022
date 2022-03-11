6bd52db6a485898094c745097bf6f1dc43ab5560
Sol Breslin <solomonbreslin@gmail.com>
2022-03-11 18:20:21 +0000

    Leaflet css overrides

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,10 +32,10 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#3865ad`,
+        background_color: `#060b13`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
-        theme_color: `#3865ad`,
+        theme_color: `#060b13`,
         display: `minimal-ui`,
         icon: `src/images/rb-icon.png`, // This path is relative to the root of the site.
       },
