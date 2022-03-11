3e94ee9f84f657c90dc87e92c308664ef2fdb0e6
Sol Breslin <solomonbreslin@gmail.com>
2022-03-11 18:06:57 +0000

    Update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,12 +32,12 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
+        background_color: `#3865ad`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
+        theme_color: `#3865ad`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/rb-icon.png`, // This path is relative to the root of the site.
       },
     },
     {
